399a0f47d786145791a98132380d9ca5ae69c7c1
Martin Santangelo <msantang78@gmail.com>
2018-05-25 18:35:23 -0300

    (feat) big images, large icon and grouping added

--- src/Services/Google/Google.php
+++ src/Services/Google/Google.php
@@ -7,22 +7,22 @@ namespace Surge\Services\Google;
 use Surge\Components\Interfaces;
 
 class Google implements Interfaces\ServiceInterface{
-    
+
     private $api_key;
     private $host = 'https://android.googleapis.com/gcm/send';
-    
+
     public function __construct($options = array()){
         if(isset($options['api_key']))
             $this->api_key = $options['api_key'];
 
     }
-    
+
     public function request($message){
         $headers = array(
             'Authorization: key=' . $this->api_key,
             'Content-Type: application/json'
             );
-            
+
         $ch = curl_init();
         curl_setopt( $ch,CURLOPT_URL, $this->host );
         curl_setopt( $ch,CURLOPT_POST, true );
@@ -32,9 +32,10 @@ class Google implements Interfaces\ServiceInterface{
         curl_setopt( $ch,CURLOPT_POSTFIELDS, $message);
         $result = curl_exec($ch );
         curl_close( $ch );
-        return $result; 
+
+        return $result;
     }
-    
+
     /**
      * Compile the message and send to the APNS service
      * @param Interfaces\MessageInterface $message
@@ -45,5 +46,5 @@ class Google implements Interfaces\ServiceInterface{
         $json = $message->toJSON();
         $this->request($json);
     }
-    
-}   
+
+}
